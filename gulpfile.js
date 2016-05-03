356ed1a954593ef7dfc8e1393d4a2078427d6d5b
AlanFall <digitalgrief@gmail.com>
2016-05-03 19:46:01 +0300

    Gulpfile fixes

--- gulpfile.js
+++ gulpfile.js
@@ -12,7 +12,7 @@ var
 	images = "./images/*",
 	lib = "./js/lib/*.js",
 	js = "./js/*.js",
-	sass = "sass/**/*.scss";
+	sassDir = "./sass/**/*.scss";
 
 
 
@@ -36,7 +36,7 @@ gulp.task('uglifyjs', function() {
 });
 
 gulp.task('sass', function() {
-	gulp.src(sass)
+	gulp.src(sassDir)
 		.pipe(sourcemaps.init())
 		.pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
 		.pipe(autoprefixer('last 2 version', 'safari 5', 'ie 7', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
@@ -46,8 +46,8 @@ gulp.task('sass', function() {
 
 gulp.task('watch', function() {
 	livereload.listen();
-	gulp.watch('js',['uglifyjs']);
-	gulp.watch('sass',['sass']);
+	gulp.watch(js,['uglifyjs']);
+	gulp.watch(sassDir,['sass']);
 	gulp.watch(['style.css', '*.php', js, 'parts/**/*.php'], function (files){
 		livereload.changed(files);
 	});
